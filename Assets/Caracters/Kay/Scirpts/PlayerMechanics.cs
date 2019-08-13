5835e560dfe2439be247ed7e19de646a51037255
Jules321 <Jules321@users.noreply.github.com>
2019-08-13 16:08:34 +0200

    Basic death mechanic.
    Added scripts to make death mechnic.

---
+++ Assets/Caracters/Kay/Scirpts/PlayerMechanics.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMechanics : IDie
+{
+    public override void Die()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
---
+++ Assets/GameMechanics/Death/DeadTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        IDie dead = collision.GetComponent<IDie>();
+
+        if (dead)
+        {
+            dead.Die();
+        }
+
+    }
+}
