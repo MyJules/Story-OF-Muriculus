100e99e4f70f247ce9be5d1a090e129ee60d90b3
Jules321 <Jules321@users.noreply.github.com>
2019-09-07 13:31:14 +0200

    Simple Sound
    Added simple sound sistem to ground.

--- Assets/Characters/Player/Scirpts/PlayerAnimation.cs
+++ Assets/Characters/Player/Scirpts/PlayerAnimation.cs
@@ -120,7 +120,7 @@ public class PlayerAnimation : MonoBehaviour
     private IEnumerator AnimatedFlip()
     {
         _animator.SetBool("isFlipTransition", true);
-        yield return new WaitWhile(() => _animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1f && Mathf.Abs(_moveX) > 0.2f);
+        yield return new WaitWhile(() => _animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1f );
         Flip();
         _animator.SetBool("isFlipTransition", false);
     }
--- Assets/Ground/GroundParticles/Scripts/SpecialEffects.cs
+++ Assets/Ground/GroundParticles/Scripts/SpecialEffects.cs
@@ -5,4 +5,13 @@ public class SpecialEffects : MonoBehaviour
 {
     [SerializeField]
     public ParticleSystem landParticles;
+
+    [SerializeField]
+    public AudioClip stepSound;
+
+    [SerializeField]
+    public AudioClip landSound;
+
+    [SerializeField]
+    public AudioClip jumpSound;
 }
