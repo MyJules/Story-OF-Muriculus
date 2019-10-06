b73259d0f98d4db329f24ac7afd3ad64042675c3
Jules321 <Jules321@users.noreply.github.com>
2019-10-06 17:31:56 +0200

    First Try Of Main Menu

---
+++ Assets/Scenes/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
